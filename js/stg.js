2935338e1c22e8a539e637245c50542e4023ccb1
chris81605 <89143549+chris81605@users.noreply.github.com>
2025-11-18 17:16:32 +0800

    Add files via upload

--- js/stg.js
+++ js/stg.js
@@ -1,4 +1,28 @@
-simpleFrameworks.addto('BeforeLinkZone', {
-    passage: ['Elk Street'],
-    widget: 'strangeGarden_link',
-})
+(() => {
+    window.modSC2DataManager.getModLoadController().addLifeTimeCircleHook(
+        'strangeGarden',
+        {
+            ModLoaderLoadEnd: async () => {
+                const logger = window.modUtils.getLogger();
+                const maplebirchMod = window.modUtils.getMod('maplebirch');
+                const simpleMod = window.modUtils.getMod('Simple Frameworks');
+
+                if (maplebirchMod) {
+                    maplebirchFrameworks.addto('CustomLinkZone', {
+                        widget: [1, 'strangeGarden_link'], // 在链接位置2前插入
+                        passage: ['Elk Street'],
+                    });
+                    logger.log('[strange garden] Maplebirch 已註冊連結');
+                } else if (simpleMod) {
+                    simpleFrameworks.addto('BeforeLinkZone', {
+                        passage: ['Elk Street'],
+                        widget: 'strangeGarden_link',
+                    });
+                    logger.log('[strange garden] Simple Frameworks 已註冊連結');
+                } else {
+                    logger.error('[strange garden] 未檢測到 Maplebirch 或 Simple Frameworks');
+                }
+            },
+        }
+    );
+})();
